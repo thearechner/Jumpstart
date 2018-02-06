d602f44ca36dea6f5c1172c98c5b60682aa66c8b
thearechner <theaemilia@gmail.com>
2018-02-06 08:48:15 +0100

    Fix Slick CDN links

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
   $('.carousel').slick({
-    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 4000,
     arrows: false,
+    draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false
   });
